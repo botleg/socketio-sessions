862e67666929d63a33a6445884f2098085503fad
hanzel.jesheen <hanzelj@gmail.com>
2015-09-20 00:13:37 +0530

    add socket.io connection

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var app = require('koa')(),
-	server = require('http').createServer(app.callback()),
-	io = require('socket.io')(server),
-	router = require('koa-router')();
+	serve = require('koa-static');
 
 const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-router.get('/', function*(next) {
-	this.body = process.versions.node;
+app.use(serve('./public'));
+
+var server = require('http').Server(app.callback()),
+	io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+	socket.emit('server event', { hello: 'world' });
+
+	socket.on('client event', function (data) {
+		console.log(data);
+	});
 });
 
-io.on('connection', function() {
-	console.log("connection established.");
-});
-
-app.use(router.routes())
-	.use(router.allowedMethods());
-
-app.listen(PORT, IP);
+server.listen(PORT, IP);
